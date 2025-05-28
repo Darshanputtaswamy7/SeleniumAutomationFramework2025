4a2bc888db6b5806cac32a5fb6eabe7344cf1a65
Darshanputtaswamy7 <darshanputtaswamy7@gmail.com>
2025-05-28 15:59:33 +0530

    Update BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -16,50 +17,53 @@ import utils.EmailUtils;
 import utils.ExtentReportManager;
 import utils.Log;
 
-public class BaseTest {
-	
-	protected WebDriver driver;
-	protected static ExtentReports extent;
-	protected ExtentTest test;
-	
-	
-	@BeforeSuite
-	public void setupReport() {
-		extent = ExtentReportManager.getReportInstance();
-	}
-	
-	@AfterSuite
-	public void teardownReport() {
-		extent.flush();
-		//String reportPath = ExtentReportManager.reportPath;
-		//EmailUtils.sendTestReport(reportPath);
-	}
-	
-	@BeforeMethod
-	public void setUp() {
-		
-		Log.info("Starting WebDriver...");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		Log.info("Navigating to URL...");
-		driver.get("https://admin-demo.nopcommerce.com/login");
-	}
-	
-	@AfterMethod
-	public void tearDown(ITestResult result) {
-		
-		if(result.getStatus() == ITestResult.FAILURE) {
-			
-			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
-			test.fail("Test Failed.. Check Screenshot", 
-					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
-		}
-		
-		
-		if (driver != null) {
-			Log.info("Closing Browser...");
-			driver.quit();
-		}
-	}
+import java.util.UUID;
 
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected static ExtentReports extent;
+    protected ExtentTest test;
+
+    @BeforeSuite
+    public void setupReport() {
+        extent = ExtentReportManager.getReportInstance();
+    }
+
+    @AfterSuite
+    public void teardownReport() {
+        extent.flush();
+        // String reportPath = ExtentReportManager.reportPath;
+        // EmailUtils.sendTestReport(reportPath);
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        Log.info("Starting WebDriver...");
+
+        // Use a unique Chrome user data directory to avoid session conflicts
+        String uniqueProfile = "/tmp/chrome-profile-" + UUID.randomUUID();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=" + uniqueProfile);
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        Log.info("Navigating to URL...");
+        driver.get("https://admin-demo.nopcommerce.com/login");
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+            test.fail("Test Failed.. Check Screenshot",
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        }
+
+        if (driver != null) {
+            Log.info("Closing Browser...");
+            driver.quit();
+        }
+    }
 }
